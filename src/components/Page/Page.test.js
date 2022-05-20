80a73c8921ca5a28e6e1cef284ea5a7753399c2b
andre cantarino <cantarino.andre@gmail.com>
2022-05-20 11:45:51 -0300

    added Page tests

--- src/components/Page/Page.test.js
+++ src/components/Page/Page.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Page from './Page'
 
 describe('Page | snapshot', () => {
@@ -7,5 +7,20 @@ describe('Page | snapshot', () => {
     const { asFragment } = render(<Page />)
 
     expect(asFragment(<Page />)).toMatchSnapshot()
+  })
+});
+
+describe('Page | receive content', () => {
+  it('should receive content as a children', () => {
+    const contentPage = <Page><div><p>Hello World</p></div></Page>;
+    const { asFragment } = render(contentPage);
+
+    expect(asFragment(contentPage)).toMatchSnapshot()
+  })
+
+  it('should pass content as a children', () => {
+    render(<Page><div><p>Hello World</p></div></Page>)
+
+    expect(screen.getByText(/Hello World/)).toBeInTheDocument()
   })
 });
